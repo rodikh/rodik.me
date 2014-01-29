52127392520db76f03b6393641d7d3b0e7b49f38
Rodik Hanukaev <rodikh@gmail.com>
2014-01-29 14:11:24 +0200

    added new html

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
 $(document).ready(function(){
 	$('.doar_electroni').text('ro'+ String.fromCharCode(10*10) +'ik'+ String.fromCharCode('64') + 'rodik.' + String.fromCharCode('109') + 'e');
-});	
+});
