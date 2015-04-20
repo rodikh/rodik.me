2c70bf4689f9f3e55444a778e72ef2aa6e3c1be7
Rodik Hanukaev <rodik@chegg.com>
2015-04-20 22:22:44 +0300

    new bootstrap version

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
-$(document).ready(function(){
-	$('.doar_electroni').text('ro'+ String.fromCharCode(10*10) +'ik'+ String.fromCharCode('64') + 'rodik.' + String.fromCharCode('109') + 'e');
-});
+(function mailto() {
+    document.querySelector('.mailto').onclick = function (evt) {
+        console.log('clicked mailto', this, evt);
+        var href = 'mailto:',
+            name = 'rodik',
+            domain = 'rodik.me';
+        this.setAttribute('href', href + name + "@" + domain);
+
+        return true;
+    }
+}());
+
+(function particleLines() {
+    var canvas = document.querySelector('.background canvas');
+    var particleLines = new ParticleLines(canvas, {particlesAmount: 180, maxDistance: 100, maxVelocity: 0.6});
+}());
+
