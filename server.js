5c5244cb25d253d110a449c60b9b6a3e580b1340
Rodik Hanukaev <rodik@chegg.com>
2016-01-07 00:41:25 +0200

    added console

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var file = new static.Server('./');
 
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
+        console.log('serving file', request.url);
         file.serve(request, response);
     }).resume();
 }).listen(3000);
+
+console.log('Listening on server 3000 -> 80');
